42df7a6a020fb2cd270bce7584df6e20b406501c
sandhya05 <sandhyamohan90@gmail.com>
2020-03-30 14:23:38 +0530

    Modified app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Link } from "react-router-dom";
+import Books from './books'
 
 class App extends Component {
   render() {
@@ -9,18 +10,19 @@ class App extends Component {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
+            <li><Link to="/books">Books</Link></li>
           </ul>
           <hr />
   
           <Route exact path="/" component={Home} />
           <Route path="/about" component={About} />
+          <Route path="/books" component={Books} />
         </div>
       </HashRouter>
     );
   }
 }
 
-// const Home = () => <div><h2>Home</h2></div>
 const About = () => <div><h2>About</h2></div>
 
 function Home(){
