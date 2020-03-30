65cd29e5583bff3758959f0a3649f27d2a4ad853
sandhya05 <sandhyamohan90@gmail.com>
2020-03-30 10:30:23 +0530

    Updates

---
+++ precache-manifest.e6c421292b8ce5ab2db1832b5cc18286.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5797471a7b10140798e2f6bc132b2d91",
+    "url": "/ReactWebApplication/index.html"
+  },
+  {
+    "revision": "ecfa29ab024274beffd1",
+    "url": "/ReactWebApplication/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fb46bb8767d7a5ff6efd",
+    "url": "/ReactWebApplication/static/js/2.71a0200c.chunk.js"
+  },
+  {
+    "revision": "ecfa29ab024274beffd1",
+    "url": "/ReactWebApplication/static/js/main.908aa425.chunk.js"
+  },
+  {
+    "revision": "d95a61eba2a8d3eb2d3a",
+    "url": "/ReactWebApplication/static/js/runtime~main.9080b6c1.js"
+  },
+  {
+    "revision": "b60d67713489ada8e40d27adf7b897bf",
+    "url": "/ReactWebApplication/static/media/book4.b60d6771.png"
+  },
+  {
+    "revision": "9bab33ef26b34a382f33f3740337b56c",
+    "url": "/ReactWebApplication/static/media/books1.9bab33ef.jpg"
+  },
+  {
+    "revision": "e2b6dd4f86ebc66f381fb7756553f95d",
+    "url": "/ReactWebApplication/static/media/books2.e2b6dd4f.jpg"
+  },
+  {
+    "revision": "74710441f7813117885f0b7960a7f28b",
+    "url": "/ReactWebApplication/static/media/books3.74710441.jpg"
+  }
+]);
